e3998c5fdc01612a64ba3b9b6d32dbd5ffa97f81
Valentin <fritz.valentin@live.fr>
2021-01-07 09:55:12 +0100

    Clean

--- ConvexHull/main.cpp
+++ ConvexHull/main.cpp
@@ -1,16 +1,34 @@
 #include "main.h"
 
-void drawEdge(sf::RenderWindow* window, Vertex* vertex)
+int main()
 {
-	auto color = sf::Color(243, 156, 18);
-	auto nextVertex = vertex->nextVertex;
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 16;
 
-	sf::Vertex line[] = {
-			sf::Vertex(sf::Vector2f(vertex->point.x, vertex->point.y), color),
-			sf::Vertex(sf::Vector2f(nextVertex->point.x, nextVertex->point.y), color)
-	};
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "ConvexHull", sf::Style::Default, settings);
 
-	window->draw(line, 2, sf::Lines);
+	Polygon polygon = generatePolygon(100);
+
+	window.setActive();
+
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				window.close();
+		}
+
+		window.clear(sf::Color(44, 62, 80));
+
+		drawPolygonEdges(&window, &polygon);
+		drawPolygonPoints(&window, &polygon);
+
+		window.display();
+	}
+
+	return 0;
 }
 
 void drawPolygonEdges(sf::RenderWindow* window, Polygon* polygon)
@@ -18,20 +36,25 @@ void drawPolygonEdges(sf::RenderWindow* window, Polygon* polygon)
 	if (polygon->primaryVertex == nullptr)
 		return;
 
-	drawEdge(window, polygon->primaryVertex);
-	auto nextVertex = polygon->primaryVertex->nextVertex;
-	while (nextVertex != polygon->primaryVertex)
+	drawLine(window, polygon->primaryVertex->point, polygon->primaryVertex->nextVertex->point);
+	auto currentVertex = polygon->primaryVertex->nextVertex;
+	while (currentVertex != polygon->primaryVertex)
 	{
-		drawEdge(window, nextVertex);
-		nextVertex = nextVertex->nextVertex;
+		drawLine(window, currentVertex->point, currentVertex->nextVertex->point);
+		currentVertex = currentVertex->nextVertex;
 	}
 }
 
-void drawPoint(sf::RenderWindow* window, Vertex* vertex) {
-	sf::CircleShape circle(3, 12);
-	circle.setFillColor(sf::Color(46, 204, 113));
-	circle.setPosition(vertex->point.x, vertex->point.y);
-	window->draw(circle);
+void drawLine(sf::RenderWindow* window, Point from, Point to)
+{
+	auto color = sf::Color(243, 156, 18);
+
+	sf::Vertex line[] = {
+			sf::Vertex(sf::Vector2f(from.x, from.y), color),
+			sf::Vertex(sf::Vector2f(to.x, to.y), color)
+	};
+
+	window->draw(line, 2, sf::Lines);
 }
 
 void drawPolygonPoints(sf::RenderWindow* window, Polygon* polygon)
@@ -39,18 +62,27 @@ void drawPolygonPoints(sf::RenderWindow* window, Polygon* polygon)
 	if (polygon->primaryVertex == nullptr)
 		return;
 
-	drawPoint(window, polygon->primaryVertex);
+	drawPoint(window, polygon->primaryVertex->point);
 	auto nextVertex = polygon->primaryVertex->nextVertex;
 	while (nextVertex != polygon->primaryVertex)
 	{
-		drawPoint(window, nextVertex);
+		drawPoint(window, nextVertex->point);
 		nextVertex = nextVertex->nextVertex;
 	}
 }
 
+void drawPoint(sf::RenderWindow* window, Point point)
+{
+	sf::CircleShape circle(3, 12);
+	circle.setFillColor(sf::Color(46, 204, 113));
+	circle.setPosition(point.x, point.y);
+	circle.setOrigin(circle.getRadius(), circle.getRadius());
+	window->draw(circle);
+}
+
 Polygon generatePolygon(int points)
 {
-	Polygon polygon;
+	Polygon polygon{};
 	Vertex* nextVertex = nullptr;
 	srand(time(nullptr));
 
@@ -65,35 +97,4 @@ Polygon generatePolygon(int points)
 		nextVertex = polygon.addVertex(Point(x, y), nextVertex);
 	}
 	return polygon;
-}
-
-int main()
-{
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 16;
-
-	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "ConvexHull", sf::Style::Default, settings);
-
-	Polygon polygon = generatePolygon(100);
-
-	window.setActive();
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-				window.close();
-		}
-
-		window.clear(sf::Color(44, 62, 80));
-
-		//drawPolygonEdges(&window, &polygon);
-		drawPolygonPoints(&window, &polygon);
-
-		window.display();
-	}
-
-	return 0;
 }
--- ConvexHull/main.h
+++ ConvexHull/main.h
@@ -6,3 +6,11 @@
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
+
+void drawPolygonEdges(sf::RenderWindow *window, Polygon *polygon);
+void drawLine(sf::RenderWindow *window, Point from, Point to);
+
+void drawPolygonPoints(sf::RenderWindow *window, Polygon *polygon);
+void drawPoint(sf::RenderWindow *window, Point point);
+
+Polygon generatePolygon(int points);
